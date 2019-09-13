cebcab117abfaeaf8de456e9479d439bf62f2af6
Reid Eastin <s198012@students.aie.edu>
2019-09-13 12:09:50 -0500

    fixed some issues with 21 being posible

---
+++ Number guesing game.md
@@ -0,0 +1,15 @@
+|Reid Eastin|
+| :---      |
+|s198012|
+| Mock assessment |
+|Number guesing game ducumentation|
+
+## 1. Requirements
+
+1. Description of Problem
+- **Name**: Number guesing game
+
+- **Problem Statement**:
+Create a number guesing game
+
+- **Problem specifications**: It must have 
--- Number guesing game/Program.cs
+++ Number guesing game/Program.cs
@@ -14,51 +14,66 @@ namespace Number_guesing_game
 
             int guess = 0; // tells game how many tries it takes for the computer to guess
             int Computerschoice; //used to store the computers guess 
-            int maxnumber = 21; // sets the higest number the program can guess it has to be 1 highe
-            
+            int maxnumber = 21; // sets the higest number the program can guess it has to be 1 higher than max number you want computer to guess computer can guess
+            bool validchoice;
+
             int minnumber = 1;  // sets the lowest number the program can guess
             bool guessing = true; // used to tell if the while loop should be run
-            string choice;
+            string choice = "1";
             
 
-            Console.WriteLine("Think of a number between " + minnumber +  " and " + (maxnumber - 1) +". \nPress any key when done");
+            Console.WriteLine("Think of a number between " + minnumber +  " and " + (maxnumber - 1) +". \nPress any key when done"); // This tells the player the numbers set above. The - 1 in this is because the the max number it guesses is 1 less than the max number
             Console.ReadKey();
-            while (guessing)
+            while (guessing) // runs loop until guessing is set to true
             {
                 Computerschoice = random.Next(minnumber , maxnumber);
                 guess++;
-                Console.WriteLine("Is your number " + Computerschoice);
+                Console.WriteLine("\nIs your number " + Computerschoice);
                 Console.WriteLine("1: This is your number");
                 Console.WriteLine("2: My number is less than the number shown");
                 Console.WriteLine("3: My number is greater than the number shown");
-                choice = Console.ReadLine();
+                validchoice = false; // used for while loop to tell game if choice is valid
+                while (!validchoice)
+                {
+                  choice = Console.ReadLine();
+                    if (choice == "1" || choice == "2" || choice == "3")
+                    {
+                        validchoice = true;
+                    }
+                    else if (!validchoice)
+                    {
+                        Console.WriteLine("please pick a valid choice");
+                    }
+                    
+                }
+                
                 if (choice == "1")
                 {
                     Console.WriteLine("The computer guessed your number in " + guess + " tries.");
-                    guessing = false;
+                    guessing = false; // sets guessing to false so the loop ends
                     Console.ReadKey();
 
                 }
                 else if (choice == "2")
                 {
                     maxnumber = Computerschoice - 1;
-                    if (minnumber >= maxnumber)
+                    if (minnumber > maxnumber)
                     {
                         Console.WriteLine("You cheated. The computer has found out in " + guess + " guesses");
                         guessing = false;
                         Console.ReadKey();
-                        break;
+                        
                     }
                 }
                 else if (choice == "3")
                 {
                     minnumber = Computerschoice + 1;
-                    if (minnumber >= maxnumber)
+                    if ( (maxnumber - 1) < minnumber)
                     {
                         Console.WriteLine("You cheated. The computer has found out in " + guess + " guesses");
                         guessing = false;
                         Console.ReadKey();
-                        break;
+                        
                     }
                 }
                 Console.WriteLine();
